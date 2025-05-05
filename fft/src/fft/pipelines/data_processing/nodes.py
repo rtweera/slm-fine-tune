fc5f8b43c718ddee2e0c88dcf1f3356644276937
Ravindu Weerasinghe <ravindutharuka2001@gmail.com>
2025-05-05 14:29:40 +0530

    Refactor data processing configuration and update file paths for tokenized datasets

--- fft/src/fft/pipelines/data_processing/nodes.py
+++ fft/src/fft/pipelines/data_processing/nodes.py
@@ -7,7 +7,6 @@ import pandas as pd
 import typing as t
 from transformers import AutoTokenizer
 from sklearn.model_selection import train_test_split
-from transformers import BatchEncoding
 from datasets import Dataset, DatasetDict
 
 
@@ -20,24 +19,6 @@ def convert_to_dataframe(df: DatasetDict) -> pd.DataFrame:
 
 def drop_extra_cols(df: pd.DataFrame, parameters: t.Dict) -> pd.DataFrame:
     return df[parameters["keep_columns"]]
-
-
-# def split_data(df: pd.DataFrame, parameters: t.Dict) -> pd.DataFrame:
-#     X = df[[parameters["text_column"]]]
-#     y = df[[parameters["target_column"]]]
-#     inter_X, test_X, inter_y, test_y = train_test_split(
-#         X, y, test_size=parameters["test_size"], random_state=parameters["random_state"]
-#     )
-#     train_X, validation_X, train_y, validation_y = train_test_split(
-#         inter_X,
-#         inter_y,
-#         test_size=parameters["validation_size"],
-#         random_state=parameters["random_state"],
-#     )
-#     data = [train_X, validation_X, test_X, train_y, validation_y, test_y]
-#     for d in data:
-#         d.reset_index(drop=True, inplace=True)
-#     return data
 
 
 def split_data(df: pd.DataFrame, parameters: t.Dict) -> pd.DataFrame:
@@ -55,23 +36,18 @@ def split_data(df: pd.DataFrame, parameters: t.Dict) -> pd.DataFrame:
     return data
 
 
-# def tokenize_dataset(
-#     df: pd.DataFrame, tokenizer: AutoTokenizer, parameters: t.Dict
-# ) -> t.Tuple[BatchEncoding, pd.DataFrame]:
-#     texts = list(df[parameters["tokenize_column"]])
-#     tokenized = tokenizer(texts, padding=True, truncation=True, return_tensors="pt")
-#     # padding to make every text equal - for tokenizing. truncation text length is only upto model context window limit
-#     df = pd.DataFrame({k: v.numpy().tolist() for k, v in tokenized.items()})
-#     return [tokenized, df]
-
-
 def _tokenize_function(
     data: Dataset, tokenizer: AutoTokenizer, parameters: t.Dict
 ) -> Dataset:
     # padding to make every text equal - for tokenizing. truncation text length is only upto model context window limit
-    return tokenizer(
+    tokenized: Dataset =  tokenizer(
         data[parameters['tokenize_column']], padding=parameters["padding"], truncation=parameters["truncation"]
     )
+    # Add the non altered columns back to the dataset
+    # for key in data:
+    #     if key not in tokenized:
+    #         tokenized[key] = data[key]
+    return tokenized[[parameters['keep_columns']]]
 
 
 def tokenize_dataset(
