60d0273e9aa9ab970ad36cd51a0638ba770b06ca
Ravindu Weerasinghe <ravindutharuka2001@gmail.com>
2025-05-07 15:47:01 +0530

    Initialize data processing pipeline with boilerplate configuration and test files

---
+++ generic/src/generic/pipelines/data_processing/nodes.py
@@ -0,0 +1,15 @@
+"""
+This is a boilerplate pipeline 'data_processing'
+generated using Kedro 0.19.12
+"""
+
+from datasets import DatasetDict
+from utils import hf_login
+
+def load_data(dataset_name):
+
+    ...
+
+def preprocess_data():
+    ...
+
